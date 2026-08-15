5758068fd5270b129f48c62540ead080c83c817a
rishurajput36 <rishurajsingh5210@gmail.com>
2026-08-15 22:39:48 +0530

    remove duplicates elements from an array

---
+++ remove.java
@@ -0,0 +1,16 @@
+//Remove duplicate elements from an array
+import java.util.*;
+
+public class remove{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        System.out.println(set);
+    }
+}
