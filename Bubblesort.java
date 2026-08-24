b01237c71ffee54876895309473cb261c8411036
rishurajput36 <rishurajsingh5210@gmail.com>
2026-08-24 14:46:31 +0530

    Bubble sort

---
+++ Bubblesort.java
@@ -0,0 +1,20 @@
+// Bubble sort...
+public class Bubblesort{
+    public static void main(String[] args) {
+     int[]arr={64,35,25,12,22,11,90};
+     int n =arr.length;
+     for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+     }
+     System.out.println("Sorted array:");
+     for(int num:arr){
+        System.out.print(num+"");
+     }
+    }
+}
