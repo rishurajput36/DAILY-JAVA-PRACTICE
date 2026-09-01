7865e6eabb60869d6260c53e11fbd23c696527e1
rishurajput36 <rishurajsingh5210@gmail.com>
2026-09-01 14:58:20 +0530

    Valid parentheses

---
+++ ValidParentheses.java
@@ -0,0 +1,50 @@
+// Valid Parentheses...
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "{[()]}";
+
+        if (isValid(s)) {
+            System.out.println("Valid Parentheses");
+        } else {
+            System.out.println("Invalid Parentheses");
+        }
+    }
+}
