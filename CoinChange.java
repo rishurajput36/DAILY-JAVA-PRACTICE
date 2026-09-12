602a7f4c18449ad452128f6b7d1aef09626fc527
rishurajput36 <rishurajsingh5210@gmail.com>
2026-09-12 23:15:10 +0530

    Coin Change

---
+++ CoinChange.java
@@ -0,0 +1,39 @@
+// Coin Change...
+import java.util.*;
+
+public class CoinChange {
+
+    public static int coinChange(int[] coins, int amount) {
+
+        int[] dp = new int[amount + 1];
+
+        Arrays.fill(dp, amount + 1);
+
+     
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+
+            for (int coin : coins) {
+
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        if (dp[amount] > amount) {
+            return -1;
+        }
+
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+
+        System.out.println(coinChange(coins, amount));
+    }
+}
