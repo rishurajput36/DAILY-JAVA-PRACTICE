97cbc8c09880b104a619a5e0973df7828fb5ee19
rishurajput36 <rishurajsingh5210@gmail.com>
2026-09-15 16:41:42 +0530

    Edit Distance

---
+++ EditDistance.java
@@ -0,0 +1,56 @@
+// Edit Distance...
+public class EditDistance {
+
+    static int editDistance(String s1, String s2) {
+
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+      
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+     
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+      
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+
+                 
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                } else {
+
+               
+                    dp[i][j] = 1 + Math.min(
+                        dp[i][j - 1],              // Insert
+                        Math.min(
+                            dp[i - 1][j],          // Delete
+                            dp[i - 1][j - 1]       // Replace
+                        )
+                    );
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "horse";
+        String s2 = "ros";
+
+        System.out.println("Edit Distance = "
+                + editDistance(s1, s2));
+    }
+}
